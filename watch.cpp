b665a7674e4d5ade4cd684038631d88fb45f27a7
rkdalswn <oon06047@gmail.com>
2018-11-22 01:58:12 +0900

    First Source code

---
+++ watch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class Watch
+{ public:
+	int hour;
+	int minute;
+	int second;
+	Watch(int h, int m, int s);
+	void getTime();
+};
+void Watch::getTime()
+{ 
+      int hour, minute, second;
+      cout << "시간을 입력하세요.\n";
+      cin >> hour; 
+      cin >> minute;
+      cin >> second;
+
+      	cout << "현재 시간은";
+	cout << hour << "시";
+	cout << minute << "분";
+	cout << second << "초";
+}
